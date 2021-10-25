600c4962e2601a710de64aa4ff4bfd05a9e5ad0c
20WH1A0448 S. FRANCY SHARON AGAPE <20wh1a0448@bvrithyderabad.edu.in>
2021-10-25 05:32:02 +0000

    I have updated on  Assigning Screen.py

---
+++ CODE/.py
@@ -0,0 +1,17 @@
+# Team 94
+
+import turtle
+
+
+# set up screen
+wn = turtle.Screen()
+wn.title("Pong by Team 94")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+# Main loop
+while True:
+    wn.update()
+
+
