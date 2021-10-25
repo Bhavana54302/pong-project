0cdda380caf6fd2e6e2048aa3b334e7750e309b1
GADDAMEEDI HARSHITHA <20wh1a1271@bvrithyderabad.edu.in>
2021-10-25 06:21:01 +0000

    Updated boarder checking .py

--- CODE 1/.py
+++ CODE 1/.py
@@ -75,12 +75,12 @@ while True:
     ball.sety(ball.ycor() + ball.dy)
 
     # Border checking
-    if ball.ycor() > 290:
-        ball.sety(290)
+    if ball.ycor() > 250:
+        ball.sety(250)
         ball.dy *= -1
 
-    elif ball.ycor() < -290:
-        ball.sety(-290)
+    elif ball.ycor() < -250:
+        ball.sety(-250)
         ball.dy *= -1
 
     elif ball.xcor() > 390:
