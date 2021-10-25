738ee7c99969a5222843a362d11574357610b52c
20WH1A0448 S. FRANCY SHARON AGAPE <20wh1a0448@bvrithyderabad.edu.in>
2021-10-25 05:38:55 +0000

    Added the Paddle A .py

--- CODE 1/.py
+++ CODE 1/.py
@@ -10,6 +10,15 @@ wn.bgcolor("light pink")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
+# Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("black")
+paddle_a.shapesize(stretch_wid=6, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
 # Main loop
 while True:
     wn.update()
