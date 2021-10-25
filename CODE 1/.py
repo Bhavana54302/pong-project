2befda6b9b70d3e5505dc0a013993c8052280d8c
Poornima Gottimukkala <20wh1a05f6@bvrithyderabad.edu.in>
2021-10-25 15:53:30 +0000

    total project Update .py

--- CODE 1/.py
+++ CODE 1/.py
@@ -3,11 +3,13 @@
 import turtle
 import winsound
 
+a_wins = False
+b_wins = False
 
 # set up screen
 wn = turtle.Screen()
 wn.title("Pong by Team 94")
-wn.bgcolor("light pink")
+wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
@@ -16,34 +18,33 @@ score_a = 0
 score_b = 0
 score_limit = 11
 
-
 # Paddle A
-left_paddle = turtle.Turtle()
-left_paddle.speed(0)
-left_paddle.shape("square")
-left_paddle.color("black")
-left_paddle.shapesize(stretch_wid=6, stretch_len=1)
-left_paddle.penup()
-left_paddle.goto(-350, 0)
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
 
 # Paddle B
-right_paddle = turtle.Turtle()
-right_paddle.speed(0)
-right_paddle.shape("square")
-right_paddle.color("black")
-right_paddle.shapesize(stretch_wid=5, stretch_len=1)
-right_paddle.penup()
-right_paddle.goto(350, 0)
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
 
 # Ball
 ball = turtle.Turtle()
 ball.speed(0)
 ball.shape("circle")
-ball.color("black")
+ball.color("white")
 ball.penup()
 ball.goto(0, 0)
-ball.dx = 0.1
-ball.dy = 0.1
+ball.dx = 0.4
+ball.dy = 0.4
 
 # pen
 pen = turtle.Turtle()
@@ -55,32 +56,32 @@ pen.goto(0, 250)
 pen.write("Player A: 0  Player B: 0", align="center", font=("Courier", 20, "normal"))
 
 # function
-def left_paddle_up():
-    y = left_paddle.ycor()
+def paddle_a_up():
+    y = paddle_a.ycor()
     y += 20
-    left_paddle.sety(y)
+    paddle_a.sety(y)
 
-def left_paddle_down():
-    y = left_paddle.ycor()
+def paddle_a_down():
+    y = paddle_a.ycor()
     y -= 20
-    left_paddle.sety(y)
+    paddle_a.sety(y)
 
-def right_paddle_up():
-    y = right_paddle.ycor()
+def paddle_b_up():
+    y = paddle_b.ycor()
     y += 20
-    right_paddle.sety(y)
+    paddle_b.sety(y)
 
-def right_paddle_down():
-    y = right_paddle.ycor()
+def paddle_b_down():
+    y = paddle_b.ycor()
     y -= 20
-    right_paddle.sety(y)
+    paddle_b.sety(y)
 
 # keyboard binding
 wn.listen()
-wn.onkeypress(left_paddle_up, "w")
-wn.onkeypress(right_paddle_down, "s")
-wn.onkeypress(right_paddle_up, "p")
-wn.onkeypress(right_paddle_down, "l")
+wn.onkeypress(paddle_a_up, "w")
+wn.onkeypress(paddle_a_down, "s")
+wn.onkeypress(paddle_b_up, "p")
+wn.onkeypress(paddle_b_down, "l")
 
 # Main loop
 while True:
@@ -89,17 +90,6 @@ while True:
     # Moving the ball
     ball.setx(ball.xcor() + ball.dx)
     ball.sety(ball.ycor() + ball.dy)
-    
-     # Paddle and ball collisions
-    if (ball.xcor() > 340 and ball.xcor() < 350) and (
-            ball.ycor() < rightpad_b.ycor()+40 and ball.ycor()>rightpad_b.ycor()-40):
-        ball.setx(340)
-        ball.dx *= -1
-        
-            if (ball.xcor() < -340 and ball.xcor() > -350) and (
-            ball.ycor() < leftpad_a.ycor()+40 and ball.ycor()>leftpad_a.ycor()-40):
-        ball.setx(-340)
-        ball.dx *= -1
 
     # Border checking
     if ball.ycor() > 290:
@@ -117,7 +107,7 @@ while True:
         ball.dx *= -1
         score_a += 1
         pen.clear()
-        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("arial", 25, "normal"))
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 20, "normal"))
         winsound.PlaySound("borders.wav", winsound.SND_ASYNC)
 
     elif ball.xcor() < -390:
@@ -125,31 +115,41 @@ while True:
         ball.dx *= -1
         score_b += 1
         pen.clear()
-        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("arial", 25, "normal"))
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 20, "normal"))
 
-   if score_a == score_limit:
+    # Paddle and ball collisions
+    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() -40):
+        ball.setx(340)
+        ball.dx *= -1
+        winsound.PlaySound("ballsound3.wav", winsound.SND_ASYNC)
+
+    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() - 40):
+        ball.setx(-340)
+        ball.dx *= -1
+        winsound.PlaySound("ballsound3.wav", winsound.SND_ASYNC)
+
+    if score_a == score_limit:
         turtle.clearscreen()
         a_wins = True
-         winsound.PlaySound("bgsound2.wav", winsound.SND_ASYNC)
+        winsound.PlaySound("bgsound2.wav", winsound.SND_ASYNC)
         break
 
     elif score_b == score_limit:
         turtle.clearscreen()
         b_wins = True
-         winsound.PlaySound("bgsound2.wav", winsound.SND_ASYNC)
+        winsound.PlaySound("bgsound2.wav", winsound.SND_ASYNC)
         break
 
 while True:
     if a_wins:
         wn.bgcolor("black")
-        pen.goto(0,0)
+        pen.goto(0, 0)
         pen.write("--GAME OVER--\nPlayer A wins", align="center", font=("Courier", 50, "normal"))
 
     elif b_wins:
         wn.bgcolor("black")
-        pen.goto(0,0)
+        pen.goto(0, 0)
         pen.write("--GAME OVER--\nPlayer B wins", align="center", font=("Courier", 50, "normal"))
-
 
 
 
